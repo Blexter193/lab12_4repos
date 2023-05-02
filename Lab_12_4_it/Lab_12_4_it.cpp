57bc4b40a748bc75c6e67b51cae40965b7f36e0a
Blexter193 <oleksiy29032005@gmail.com>
2023-05-02 23:50:59 +0300

    Lab12_4

---
+++ Lab_12_4_it/Lab_12_4_it.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+void addToEnd(Node*& head, int data)
+{
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->next = head;
+
+    if (head == NULL)
+    {
+        head = newNode;
+        newNode->next = head;
+    }
+    else
+    {
+        Node* current = head;
+
+        while (current->next != head)
+        {
+            current = current->next;
+        }
+
+        current->next = newNode;
+    }
+}
+
+void printList(Node* head)
+{
+    Node* current = head;
+
+    if (head == NULL)
+    {
+        cout << "Список порожній!" << endl;
+        return;
+    }
+
+    do
+    {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    while (current != head);
+
+    cout << endl;
+}
+
+bool isSorted(Node* head)
+{
+    Node* current = head;
+
+    if (head == NULL)
+    {
+        return true;
+    }
+
+    do
+    {
+        if (current->data > current->next->data)
+        {
+            return false;
+        }
+
+        current = current->next;
+    }
+    while (current != head);
+
+    return true;
+}
+    
+int main()
+{
+    Node* head = NULL;
+    
+    addToEnd(head, 1);
+    addToEnd(head, 2);
+    addToEnd(head, 3);
+    addToEnd(head, 4);
+    addToEnd(head, 5);
+    
+    cout << "Список: ";
+    printList(head);
+    
+    if (isSorted(head))
+    {
+        cout << "Список посортовано!" << endl;
+    }
+    else
+    {
+        cout << "Список не посортовано!" << endl;
+    }
+    
+    return 0;
+}
+
